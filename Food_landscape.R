6129583d49e9ba64ec48fcd5477ada5e6ee1c47f
Rich Tyler <rich.tyler.2018@hotmail.com>
2022-06-16 21:54:33 +0100

    Work on lsoa api querying

--- Food_landscape.R
+++ Food_landscape.R
@@ -7,16 +7,25 @@ source_directory <- paste0(github_repo_dir, '/data')
 output_directory <- paste0(github_repo_dir, '/outputs')
 
 # Food standards agency API ####
+
+# Verity kindly helped navigate the xml structure reading into R.
 route_path <- 'http://ratings.food.gov.uk/OpenDataFiles/'
+
+# Use a loop to grab the endpoints for our areas, you could probably scrape this from the site but the eng urls do not have the district names in so might be difficult. But you could certainly scrape every path on the page (all LTLAs in England and Wales, maybe more)
 xml_paths <- c('FHRS323en-GB', 'FHRS324en-GB', 'FHRS325en-GB', 'FHRS326en-GB', 'FHRS327en-GB', 'FHRS328en-GB', 'FHRS329en-GB')
 
 for(i in 1:length(xml_paths)){
   xml_path_x <- xml_paths[i]
   assign(paste0('xmldf_', i),  
          xmlToDataFrame(nodes=getNodeSet(xmlParse(read_xml(paste0(route_path, xml_path_x, '.xml'))), "//EstablishmentDetail")) %>%
-           bind_cols(xmlToDataFrame(nodes=getNodeSet(xmlParse(read_xml(paste0(route_path, xml_path_x, '.xml'))), "//EstablishmentDetail//Geocode"))))
+           bind_cols(xmlToDataFrame(nodes=getNodeSet(xmlParse(read_xml(paste0(route_path, xml_path_x, '.xml'))), "//EstablishmentDetail//Geocode"
+))))
+  
+  # assign() allows you to automate creating named objects and is great for use in loops.
+  # The alternative would be to create a dummy empty dataframe and then within the loop keep adding the new data to it. However, there are 20+ fields and that would be a bit clunky 
 }
 
+# Bind the rows into a single df
 food_hygeine_df <- xmldf_1 %>% 
   bind_rows(xmldf_2) %>%   
   bind_rows(xmldf_3) %>%  
@@ -29,6 +38,10 @@ food_hygeine_df <- xmldf_1 %>%
          Longitude = as.numeric(Longitude)) %>% 
   mutate(Postcode = gsub(' ' ,'', PostCode))
 
+# I have been a bit lazy here and hard coded that there are seven individual dataframes (this was partly so i could see how the dataframes were coming out of the FSA site).
+
+# There are 7,800 premises here, but not all of them have geolocation information
+
 with_lat <- food_hygeine_df %>% 
   filter(!is.na(Latitude)) %>% 
   nrow() %>% 
@@ -39,12 +52,25 @@ with_postcode <- food_hygeine_df %>%
   nrow() %>% 
   as.numeric()
 
-paste0('There are ', format(with_lat, big.mark = ','), ' records with a geolocation (lat/long coordinates) recorded out of ', format(nrow(food_hygeine_df), big.mark = ','), ' records overall. This is ', round((with_lat/nrow(food_hygeine_df))*100,1) ,'%. There are ', format(with_postcode, big.mark = ','), ' records with a postcode given (', round((with_postcode/nrow(food_hygeine_df))*100,1) ,'%). This means, we can try to geolocate nearly 1,000 more records using postcode lookup from postcodesio.')
+with_geo_no_postcode <- food_hygeine_df %>% 
+  filter(is.na(PostCode) & !is.na(Latitude)) %>% 
+  nrow() %>% 
+  as.numeric()
+
+paste0('There are ', format(with_lat, big.mark = ','), ' records with a geolocation (lat/long coordinates) provided out of ', format(nrow(food_hygeine_df), big.mark = ','), ' records overall. This is ', round((with_lat/nrow(food_hygeine_df))*100,1) ,'%. There are ', format(with_postcode, big.mark = ','), ' records with a postcode given (', round((with_postcode/nrow(food_hygeine_df))*100,1) ,'%). There are also ',   format(with_geo_no_postcode, big.mark = ',') , ' records with no postcode given but a geocode. As a result, we need to do some work to geolocate nearly 1,000 more records using postcode lookup from postcodesio as well as the given geolocation data from the original source.')
+
+
+# Postcode lookup ####
+
+# PostcodesioR is a wrapper function for the postcodesio api, a free, open source geolocation api for the UK. This uses the OS and ONS publicly available data. 
+
+# It is convered by the Open Government Licence. It is updated quarterly to reflect new postcodes and terminated postcodes.
+
+# The point is given the coordinates of the nearest delivery point to the calculated mean position of the delivery points within the postcode unit. This is the notional position of the postcode
 
 fhrs_postcodes <- food_hygeine_df %>% 
   select(PostCode, Postcode) %>% 
-  unique() #%>% 
-  mutate(Postcode = gsub(' ' ,'', PostCode)) %>% 
+  unique() %>% 
   filter(!is.na(PostCode))
 
 lookup_result <- data.frame(postcode = character(), longitude = double(), latitude = double(), lsoa_code = character(), msoa_code = character(), msoa = character())
@@ -60,6 +86,8 @@ for(i in 1:nrow(fhrs_postcodes)){
 uncaught_postcodes <- lookup_result %>% 
   filter(is.na(latitude)) %>% 
   filter(postcode != 'BN113SE')
+
+paste0('No postcode data were available for ', nrow(uncaught_postcodes), ' postcodes. These may have been terminated postcodes.')
 
 lookup_result <- lookup_result %>% 
   filter(!postcode %in% uncaught_postcodes$postcode) 
@@ -84,29 +112,110 @@ lookup_result_final <- lookup_result %>%
   rename(lookup_latitude = latitude,
          lookup_longitude = longitude)
 
-food_hygeine_df_1 <- food_hygeine_df %>% 
-  mutate(Postcode = gsub(' ', '', PostCode)) %>% 
-  left_join(lookup_result_final, by = 'Postcode')
- 
-food_hygeine_df_1 %>% 
-  filter(is.na(latitude) & is.na(lookup_latitude)) %>% 
+# Now we have postcode results for all the postcodes we could find in the FHRS dataset. 
+
+# We want to make sure as many records as possible have geolocation data. In most cases there will be two values to choose (geolocation from FSA, and postcode look up). I am going to favour the geolocation supplied by FSA, where this is unavailable and a postcode lookup based geolocation is available we will use it.
+
+
+# Whilst the postcodeior tool gave us lsoa data, I want to rebuild this from the GIS coordinates to be consistent. To do this we need to draw in LSOA data.
+
+# We will assign premises to an LSOA and from there append information on deprivation (IMD), population (mid 2020), as well as MSOA, and LTLA. We should build a lookup table first.
+
+# read in national deprivation deciles
+
+# Verity cleverly used the select clause to rename the fields at the same time (mind blown emoji)
+lsoa_lookup <- read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/845345/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", col_types = cols(`LSOA code (2011)` = col_character(), `Index of Multiple Deprivation (IMD) Score` = col_number(), `Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)` = col_number(), `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)` = col_integer())) %>%
+  select(LSOA11CD = `LSOA code (2011)`,
+         IMD_Score = `Index of Multiple Deprivation (IMD) Score`,
+         IMD_Decile = `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`,
+         LTLA = `Local Authority District name (2019)`) %>% 
+  filter(LTLA %in% c('Adur', 'Arun', 'Chichester', 'Crawley', 'Horsham', 'Mid Sussex', 'Worthing')) 
+
+lsoa_clipped_spdf <- geojson_read('https://opendata.arcgis.com/datasets/e9d10c36ebed4ff3865c4389c2c98827_0.geojson',  what = "sp") %>% 
+  filter(LSOA11CD %in% lsoa_lookup$LSOA11CD) 
+
+
+# oa_lookup <- GET('https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/OA11_LSOA11_MSOA11_LAD_GB_LU_classificationv2/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson')
+
+#https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BFE_V2/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson
+
+
+query <- "https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BFE_V2/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+
+
+# Thanks to Verity and the traffordDatalab and some work I did on SID (buidling strings) for api build ideas 
+# https://medium.com/@traffordDataLab/pushing-the-boundaries-with-the-open-geography-portal-api-4d70637bddc3
+
+# You must escape % with a preceding %
+# space_notation <- '%20'
+# comma_notation <- '%27'
+
+# You can just about get 101 records to paste together, so we only need five groups.
+search_string <- gsub(' ', '', toString(sprintf("%%27%s%%20%%27", sample(lsoa_lookup$LSOA11CD, 101))))
+
+query_x1<- paste0('https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BFE_V2/FeatureServer/0/query?where=LSOA11CD%20IN%20(', search_string ,')&outFields=*&outSR=4326&f=geojson')
+
+# Write the sf (simple features) object to geojson, then read the geojson file using geojson_read() to returm a spatial polygons dataframe. This is the easiest type to work with that I am familiar with. Then use as required.
+st_write(st_read(query_x1), paste0(output_directory, "/lsoa_fe_2011.geojson"), delete_dsn = TRUE)
+lsoa_df <- geojson_read(paste0(output_directory, "/lsoa_fe_2011.geojson"), what = 'sp')
+plot(lsoa_df)
+
+
+
+
+
+
+
+
+
+food_hygeine_df_final <- food_hygeine_df %>% 
+  left_join(lookup_result_final[c('Postcode', 'lookup_longitude', 'lookup_latitude')], by = 'Postcode') %>% 
+  mutate(final_latitude = ifelse(is.na(Latitude), lookup_latitude, Latitude),
+         final_longitude = ifelse(is.na(Longitude), lookup_longitude, Longitude)) %>% 
+  mutate(final_latitude = ifelse(FHRSID == '917215', 50.7823345, ifelse(FHRSID == '1087421', 50.8354062, ifelse(FHRSID == '190553', 51.115741, ifelse(FHRSID == '530805', 50.8129842, ifelse(FHRSID == '1439385', 51.0051779, final_latitude)))))) %>% 
+  mutate(final_longitude = ifelse(FHRSID== '917215', -0.6714866, ifelse(FHRSID == '1087421', -0.7738526, ifelse(FHRSID == '190553', -0.1851306, ifelse(FHRSID == '530805', -0.4834992, ifelse(FHRSID == '1439385', -0.1688246, final_longitude))))))
+
+original_lat <- food_hygeine_df_final %>% 
+  filter(!is.na(Latitude)) %>% 
+  nrow()
+
+final_lat <- food_hygeine_df_final %>% 
+  filter(!is.na(final_latitude)) %>% 
+  nrow()
+
+paste0('The original dataset had geolocation data for ', format(original_lat, big.mark = ','), ' records (', round((original_lat / nrow(food_hygeine_df_final))*100,1), '% of records. We have increased the proportion of records with geolocation data to ', round((final_lat / nrow(food_hygeine_df_final))*100,1),  ', ', format(final_lat, big.mark = ','), ' records.')
+
+
+leaflet(lsoa_df) %>% 
+  addTiles() %>% 
+  addPolygons(stroke = TRUE,
+              color = 'purple',
+              weight = 3,
+              smoothFactor = 0.3, 
+              fillOpacity = 0) %>% 
+  addCircleMarkers(data = food_hygeine_df_final,
+                   lat = ~final_latitude,
+                   lng = ~final_longitude,
+                   popup = paste0(food_hygeine_df_final$BusinessName, ' ', food_hygeine_df_final$LocalAuthorityName, '<br><br>', food_hygeine_df_final$FHRSID),
+                   stroke = FALSE,
+                   fillOpacity = 1,
+                   radius = 4)
+
+food_hygeine_df_final %>% 
   view()
 
 
-unique(final_food_hygeine_df$BusinessType)
+plot(lsoa_clipped_spdf)
 
 
-# read in national deprivation deciles
-imd <- read_csv("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/845345/File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv", col_types = cols(`LSOA code (2011)` = col_character(), `Index of Multiple Deprivation (IMD) Score` = col_number(), `Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)` = col_number(), `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)` = col_integer())) %>%
-  select(LSOA11CD = `LSOA code (2011)`,
-         IMD_Score = `Index of Multiple Deprivation (IMD) Score`,
-         IMD_Decile = `Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)`)
+# tidy up environment
+rm(xmldf_1, xmldf_2, xmldf_3, xmldf_4, xmldf_5, xmldf_6, xmldf_7, lookup_result, lookup_result_x, lookup_result_uncaught, fhrs_postcodes)
+
+
 
 chosen_outlets <- final_food_hygeine_df %>% 
   filter(BusinessType %in% c('Restaurant/Cafe/Canteen', 'Mobile caterer', 'Takeaway/sandwich shop')) %>% 
-  mutate(latitude = ifelse(FHRSID == '917215', 50.7823345, ifelse(FHRSID == '1087421', 50.8354062, latitude))) %>% 
-  mutate(longitude = ifelse(FHRSID== '917215', -0.6714866, ifelse(FHRSID == '1087421', -0.7738526, longitude))) %>% 
-  filter(!is.na(latitude))
+ 
 
 
 ### Get long and lat from your data.frame. Make sure that the order is in lon/lat.
@@ -131,17 +240,6 @@ geojson_write(geojson_json(chosen_outlets_spdf), file = paste0(output_directory,
 # System or checks in place to ensure that food sold or served is safe to eat, evidence that staff know about food safety, and the food safety officer has confidence that standards will be maintained in future.
 # 
 
-leaflet() %>% 
-  addTiles() %>% 
-  addCircleMarkers(data = chosen_outlets,
-             lat = ~latitude,
-             lng = ~longitude,
-             popup = paste0(chosen_outlets$BusinessName, ' ', chosen_outlets$LocalAuthorityName),
-             stroke = FALSE,
-             fillOpacity = 1,
-             radius = 4)
-
-addCircleMarkers()
 
  # addPolygons(data = LSOAs_map, stroke = TRUE, weight = 2, color = "#1a0dab", fill = "#1a0dab", fillOpacity = 0.2, popup = paste("LSOA Name: ",LSOAs_map@data$lsoa11nm, "<br>LSOA Code: ", LSOAs_map@data$lsoa11cd, sep = ""), group = "Show LSOAs") %>% 
   # addLayersControl(baseGroups = c("Show LSOAs", "Show boundary"),overlayGroups = c("Show GPs"),options = layersControlOptions(collapsed = FALSE)) %>%
